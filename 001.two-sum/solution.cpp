45979f9a67913f99319fbfeafcc0ef265e2c70c4
steven <zenggang@bangwork.com>
2016-11-22 09:11:09 +0800

    Change 001

--- 001.two-sum/solution.cpp
+++ 001.two-sum/solution.cpp
@@ -5,17 +5,20 @@
 #include "solution.h"
 #include <iostream>
 #include <assert.h>
+#include <unordered_map>
 
 std::vector<int> Solution::twoSum(std::vector<int> &nums, int target) {
     std::vector<int> result;
+    std::unordered_map<int, int> hash;
+
     for (int i = 0; i < nums.size(); ++i) {
-        for (int j = i + 1; j < nums.size(); ++j) {
-            if (nums[i] + nums[j] == target) {
-                result.push_back(i);
-                result.push_back(j);
-                return result;
-            }
+        auto numToFind = target - nums[i];
+        if (hash.find(numToFind) != hash.end()) {
+            result.push_back(hash[numToFind]);
+            result.push_back(i);
+            return result;
         }
+        hash[nums[i]] = i;
     }
     assert(false && "Unreachable");
     return result;
