b95733adff6be5d08665ab1db522247d7ed30739
steven <zenggang@bangwork.com>
2016-11-24 19:37:48 +0800

    Add 005

---
+++ src/005.longest-palindromic-substring/solution.h
@@ -0,0 +1,12 @@
+#ifndef LEETCODE_005_SOLUTION_H_
+#define LEETCODE_005_SOLUTION_H_
+
+#include <string>
+
+class solution {
+public:
+    std::string longestPalindrome(std::string s);
+};
+
+
+#endif // LEETCODE_005_SOLUTION_H_
