ae8d793a1cda6372bdb1f39bf2dcecfd7236a967
steven <zenggang@bangwork.com>
2016-11-24 22:09:31 +0800

    优化

--- src/006.zigzag-conversion/solution.cpp
+++ src/006.zigzag-conversion/solution.cpp
@@ -1,33 +1,25 @@
 #include "solution.h"
 
+#include <vector>
+#include <string>
 #include <iostream>
 
-// "0123456789"  5
+// "0123456789"  4
 std::string solution::convert(std::string s, int numRows) {
-    size_t length = s.length();
+    int length = s.length();
     if (numRows <= 1 || numRows >= length) {
         return s;
     }
     std::string output;
-    for (int row = 0; row < numRows; ++row) {
-        output += s[row];
 
-        int firstStep = 0;
-        int secondStep = 0;
-        if (row % (numRows - 1) == 0) {
-            firstStep = 2 * numRows - 2;
-        } else {
-            firstStep = 2 * numRows - 2 - 2 * row;
-            secondStep = 2 * row;
-        }
-        for (size_t i = row; i < length;) {
-            if (firstStep > 0) {
-                i += firstStep;
-                if (i < length) output += s[i];
-            }
-            if (secondStep > 0) {
-                i += secondStep;
-                if (i < length) output += s[i];
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = i; j < length; j += 2 * (numRows - 1)) {
+            output += s[j];
+            if (i != 0 && i != numRows - 1) {
+                int t = j + 2 * (numRows - 1) - 2 * i;
+                if (t < length) {
+                    output += s[t];
+                }
             }
         }
     }
