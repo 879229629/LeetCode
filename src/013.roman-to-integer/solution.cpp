e1d18f9b34c3f032266e31a1e352a2f41aa8d11a
steven <zenggang@bangwork.com>
2016-11-27 14:23:21 +0800

    优化 013

--- src/013.roman-to-integer/solution.cpp
+++ src/013.roman-to-integer/solution.cpp
@@ -1,101 +1,23 @@
 #include "solution.h"
 
-#include <vector>
+#include <unordered_map>
+#include <iostream>
 
 int solution::romanToInt(std::string s) {
-    size_t length = s.length();
-    if (length == 0) return 0;
-    if (s[0] == 'M') {
-        int count = 1;
-        int i = 1;
-        while (i < length && s[i] == 'M') {
-            ++count;
-            ++i;
-        }
-        return count * 1000 + romanToInt(s.substr(i));
-    } else if (s[0] == 'C') {
-        if (3 <= length && s.substr(0, 3) == "CCC") {
-            return 300 + romanToInt(s.substr(3));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "CM") {
-            return 900 + romanToInt(s.substr(2));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "CD") {
-            return 400 + romanToInt(s.substr(2));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "CC") {
-            return 200 + romanToInt(s.substr(2));
-        }
-        return 100 + romanToInt(s.substr(1));
-    } else if (s[0] == 'D') {
-        if (4 <= length && s.substr(0, 3) == "DCCC") {
-            return 800 + romanToInt(s.substr(4));
-        }
-        if (3 <= length && s.substr(0, 3) == "DCC") {
-            return 700 + romanToInt(s.substr(3));
-        }
-        if (2 <= length && s.substr(0, 2) == "DC") {
-            return 600 + romanToInt(s.substr(2));
-        }
-        return 500 + romanToInt(s.substr(1));
-    } else if (s[0] == 'X') {
-        if (3 <= length && s.substr(0, 3) == "XXX") {
-            return 30 + romanToInt(s.substr(3));
-        }
-        if (2 <= length && s.substr(0, 2) == "XC") {
-            return 90 + romanToInt(s.substr(2));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "XL") {
-            return 40 + romanToInt(s.substr(2));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "XX") {
-            return 20 + romanToInt(s.substr(2));
-        }
-        return 10 + romanToInt(s.substr(1));
-    } else if (s[0] == 'L') {
-        if (4 <= length && s.substr(0, 4) == "LXXX") {
-            return 80 + romanToInt(s.substr(4));
-        }
-        if (3 <= length && s.substr(0, 3) == "LXX") {
-            return 70 + romanToInt(s.substr(3));
-        }
-        if (2 <= length && s.substr(0, 2) == "LX") {
-            return 60 + romanToInt(s.substr(2));
-        }
-        return 50 + romanToInt(s.substr(1));
-    } else if (s[0] == 'I') {
-        if (3 <= length && s.substr(0, 3) == "III") {
-            return 3 + romanToInt(s.substr(3));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "IX") {
-            return 9 + romanToInt(s.substr(2));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "IV") {
-            return 4 + romanToInt(s.substr(2));
-        }
-
-        if (2 <= length && s.substr(0, 2) == "II") {
-            return 2 + romanToInt(s.substr(2));
-        }
-        return 1 + romanToInt(s.substr(1));
-    } else if (s[0] == 'V') {
-        if (4 <= length && s.substr(0, 4) == "VIII") {
-            return 8 + romanToInt(s.substr(4));
-        }
-        if (3 <= length && s.substr(0, 3) == "VII") {
-            return 7 + romanToInt(s.substr(3));
-        }
-        if (2 <= length && s.substr(0, 2) == "VI") {
-            return 6 + romanToInt(s.substr(2));
-        }
-        return 5 + romanToInt(s.substr(1));
+    std::unordered_map<char, int> mp = {{'I', 1},
+                                        {'V', 5},
+                                        {'X', 10},
+                                        {'L', 50},
+                                        {'C', 100},
+                                        {'D', 500},
+                                        {'M', 1000}};
+    int res = 0;
+    int i = 0;
+    for (i = 0; i < s.length() - 1; ++i) {
+        if (mp[s[i]] >= mp[s[i + 1]]) res += mp[s[i]];
+        else res -= mp[s[i]];
+        std::cout << res << std::endl;
     }
-    return 0;
+    res += mp[s[i]];
+    return res;
 }
