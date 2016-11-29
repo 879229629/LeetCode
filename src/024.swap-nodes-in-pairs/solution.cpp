e17eff8e8076535c6653f9374e7663a93b02ef42
steven <zenggang@bangwork.com>
2016-11-29 18:53:48 +0800

    优化 024

--- src/024.swap-nodes-in-pairs/solution.cpp
+++ src/024.swap-nodes-in-pairs/solution.cpp
@@ -5,22 +5,10 @@
 ListNode *solution::swapPairs(ListNode *head) {
     if (head == nullptr) return nullptr;
     if (head->next == nullptr) return head;
-    ListNode *newHead = head->next;
+    ListNode *next = head->next;
 
-    ListNode *l = nullptr;
-    ListNode *r = head;
-    while (r) {
-        ListNode *nextNode = r->next;
-        if (!nextNode) break;
-        if (l) {
-            l->next = nextNode;
-        } else {
-            l = nextNode;
-        }
-        r->next = nextNode->next;
-        nextNode->next = r;
-        l = r;
-        r = r->next;
-    }
-    return newHead;
+    head->next = swapPairs(next->next);
+    next->next = head;
+
+    return next;
 }
