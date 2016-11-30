d1df35099fc2db4181afd2171227bf331a23b824
steven <zenggang@bangwork.com>
2016-11-30 08:38:03 +0800

    优化 026

--- src/026.remove-duplicates-from-sorted-array/solution.cpp
+++ src/026.remove-duplicates-from-sorted-array/solution.cpp
@@ -1,14 +1,12 @@
 #include "solution.h"
 
 int solution::removeDuplicates(std::vector<int> &nums) {
-    int len = 0;
-    for (int i = 0; i < nums.size();) {
-        nums[len] = nums[i];
-        ++len;
-        ++i;
-        while (nums[i] == nums[i - 1] && i < nums.size()) {
-            ++i;
-        }
+    int length = nums.size();
+    if (length < 2) return length;
+
+    int len = 1;
+    for (int i = 1; i < nums.size(); ++i) {
+        if (nums[i] != nums[i - 1]) nums[len++] = nums[i];
     }
     return len;
 }
