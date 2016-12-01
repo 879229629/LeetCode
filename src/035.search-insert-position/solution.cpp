2a81e7e3252dc0a7e1c1de65f98fd41a68a2bde0
steven <zenggang@bangwork.com>
2016-12-01 19:44:29 +0800

    Add 035

---
+++ src/035.search-insert-position/solution.cpp
@@ -0,0 +1,6 @@
+#include "solution.h"
+
+int solution::searchInsert(std::vector<int> &nums, int target) {
+
+    return 1;
+}
---
+++ src/035.search-insert-position/solution.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_035_SOLUTION_H_
+#define LEETCODE_035_SOLUTION_H_
+
+#include <vector>
+
+class solution {
+public:
+    int searchInsert(std::vector<int> &nums, int target);
+
+};
+
+
+#endif // LEETCODE_035_SOLUTION_H_
