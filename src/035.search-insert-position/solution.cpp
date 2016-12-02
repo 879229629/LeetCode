5616c1e1d1a7107a12d61c35c30e9d203882a51e
steven <zenggang@bangwork.com>
2016-12-02 08:40:56 +0800

    Add 035

--- src/035.search-insert-position/solution.cpp
+++ src/035.search-insert-position/solution.cpp
@@ -1,6 +1,17 @@
 #include "solution.h"
 
 int solution::searchInsert(std::vector<int> &nums, int target) {
+    int i = 0;
+    int j = nums.size() - 1;
+    while (i <= j) {
+        int mid = i + (j - i) / 2;
+        if (nums[mid] == target) return mid;
 
-    return 1;
+        if (nums[mid] < target) {
+            i = mid + 1;
+        } else {
+            j = mid - 1;
+        }
+    }
+    return i;
 }
