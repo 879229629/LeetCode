6071fb75bc8343731eb55a82ea44bce5327de206
steven <zenggang@bangwork.com>
2016-12-03 13:46:20 +0800

    优化 041

--- src/041.first-missing-positive/solution.cpp
+++ src/041.first-missing-positive/solution.cpp
@@ -1,12 +1,16 @@
 #include "solution.h"
 
 int solution::firstMissingPositive(std::vector<int> &nums) {
-    std::sort(nums.begin(), nums.end());
-    int ret = 1;
     for (int i = 0; i < nums.size(); ++i) {
-        if (nums[i] < 1) continue;
-        if (nums[i] > ret) break;
-        ret = nums[i]+1;
+        while (nums[i] > 0 && nums[i] <= nums.size() && nums[i] != nums[nums[i] - 1]) {
+            std::swap(nums[i], nums[nums[i] - 1]);
+        }
     }
-    return ret;
+
+    for (int j = 0; j < nums.size(); ++j) {
+        if (nums[j] != j + 1) {
+            return j + 1;
+        }
+    }
+    return nums.size() + 1;
 }
