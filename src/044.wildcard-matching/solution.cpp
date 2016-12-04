5c873b661baa077e07a9c3d223da5165a7e47c7e
steven <zenggang@bangwork.com>
2016-12-04 22:13:56 +0800

    优化

--- src/044.wildcard-matching/solution.cpp
+++ src/044.wildcard-matching/solution.cpp
@@ -4,18 +4,30 @@
 
 bool solution::isMatch(char *s, char *p) {
     if (*p == '\0') return *s == '\0';
-    std::cout << s << "][" << p << std::endl;
-    if (*p == '*') {
-        while (*p == '*') ++p;
-        while (*s) {
-            if (isMatch(s++, p)) return true;
-        }
-        return isMatch(s++, p);
-    } else {
-        if (*s == *p || (*p == '?' && *s != '\0')) {
-            return isMatch(s + 1, p + 1);
+
+    char *s1 = NULL;
+    char *p1 = NULL;
+    bool found = false;
+    while (*s != '\0') {
+        if (*p == '?') {
+            ++s;
+            ++p;
+        } else if (*p == '*') {
+            found = true;
+            s1 = s;
+            p1 = ++p;
         } else {
-            return false;
+            if (*s == *p) {
+                ++s;
+                ++p;
+            } else if (found) {
+                s = ++s1;
+                p = p1;
+            } else {
+                return false;
+            }
         }
     }
+    while (*p == '*') ++p;
+    return *p == '\0';
 }
