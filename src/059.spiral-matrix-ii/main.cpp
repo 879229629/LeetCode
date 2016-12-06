2b7956bd53ebeb84ebb14166aadf1cd20b9e13b5
steven <zenggang@bangwork.com>
2016-12-06 19:10:41 +0800

    add 059

--- src/059.spiral-matrix-ii/main.cpp
+++ src/059.spiral-matrix-ii/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <iomanip>
+
 
 #include "solution.h"
 
 int main(int argc, char *argv[]) {
     solution s;
     std::vector<std::vector<int>> nums = s.generateMatrix(9);
+    for (int i = 0; i < nums.size(); ++i) {
+        for (int j = 0; j < nums[i].size(); ++j) {
+            std::cout << std::setw(4) << nums[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
--- src/059.spiral-matrix-ii/solution.cpp
+++ src/059.spiral-matrix-ii/solution.cpp
@@ -1,5 +1,40 @@
 #include "solution.h"
 
+#include <iostream>
+
 std::vector<std::vector<int>> solution::generateMatrix(int n) {
-    return {};
+    std::vector<int> tmp(n, 0);
+    std::vector<std::vector<int>> result(n, tmp);
+
+    int row = 0;
+    int column = 0;
+    int m = n - 1;
+    n = n - 1;
+
+    int k = 1;
+
+    while (true) {
+//        std::cout << row << "," << column << "," << m << "," << n << std::endl;
+        for (int i = column; i <= n; ++i) {
+            result[row][i] = k++;
+        }
+        if (++row > m) break;
+
+        for (int j = row; j <= m; ++j) {
+            result[j][n] = k++;
+        }
+        if (--n < column) break;
+
+        for (int l = n; l >= column; --l) {
+            result[m][l] = k++;
+        }
+        if (--m < row) break;
+
+        for (int i = m; i >= row; --i) {
+            result[i][column] = k++;
+        }
+        if (++column > n) break;
+    }
+
+    return result;
 }
