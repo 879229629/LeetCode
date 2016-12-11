b138f50bcb3c5bb61b0890d5496d1172a675baf8
steven <zenggang@bangwork.com>
2016-12-11 20:03:55 +0800

    add 082

---
+++ src/061-120/082.remove-duplicates-from-sorted-list-ii/solution.h
@@ -0,0 +1,12 @@
+#ifndef LEETCODE_082_SOLUTION_H_
+#define LEETCODE_082_SOLUTION_H_
+
+#include "../../common/listnode.h"
+
+class solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head);
+};
+
+
+#endif // LEETCODE_082_SOLUTION_H_
