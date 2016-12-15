608756b4ff9645d20c65556b639fb449b1a54c91
steven <zenggang@bangwork.com>
2016-12-15 19:02:25 +0800

    add 102

---
+++ src/061-120/102.binary-tree-level-order-traversal/solution.cpp
@@ -0,0 +1,21 @@
+#include "solution.h"
+
+std::vector<std::vector<int>> solution::levelOrder(TreeNode *root) {
+    if(!root) return {};
+    std::vector<std::vector<int>> result;
+    std::vector<TreeNode *> s;
+    s.push_back(root);
+    int i = 0;
+    while (i < s.size()) {
+        int to = s.size();
+        std::vector<int> nums;
+        while (i < to) {
+            nums.push_back(s[i]->val);
+            if (s[i]->left) s.push_back(s[i]->left);
+            if (s[i]->right) s.push_back(s[i]->right);
+            ++i;
+        }
+        result.push_back(nums);
+    }
+    return result;
+}
---
+++ src/061-120/102.binary-tree-level-order-traversal/solution.h
@@ -0,0 +1,13 @@
+#ifndef LEETCODE_102_SOLUTION_H_
+#define LEETCODE_102_SOLUTION_H_
+
+#include <vector>
+#include "../../common/treeNode.h"
+
+class solution {
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode* root);
+};
+
+
+#endif // LEETCODE_102_SOLUTION_H_
