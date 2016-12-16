ba0386fcc75920c6aa6e9694f4e805953c89984e
steven <zenggang@bangwork.com>
2016-12-16 09:07:53 +0800

    add 104

---
+++ src/061-120/104.maximum-depth-of-binary-tree/solution.cpp
@@ -0,0 +1,5 @@
+#include "solution.h"
+
+int solution::maxDepth(TreeNode *root) {
+    return 0;
+}
---
+++ src/061-120/104.maximum-depth-of-binary-tree/solution.h
@@ -0,0 +1,12 @@
+#ifndef LEETCODE_104_SOLUTION_H_
+#define LEETCODE_104_SOLUTION_H_
+
+#include "../../common/treeNode.h"
+
+class solution {
+public:
+    int maxDepth(TreeNode* root);
+};
+
+
+#endif // LEETCODE_104_SOLUTION_H_
