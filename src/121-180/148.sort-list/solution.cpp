385eb1656d77952a3bd8d44d011e93a30225212e
steven <zenggang@bangwork.com>
2016-12-21 23:39:17 +0800

    add 148

--- src/121-180/148.sort-list/solution.cpp
+++ src/121-180/148.sort-list/solution.cpp
@@ -38,7 +38,36 @@ ListNode *solution::merge(ListNode *s1, ListNode *s2) {
         }
     }
 
-    while (s1 != nullptr) {
+    while (s1 != nullptr) {ListNode *solution::merge(ListNode *s1, ListNode *s2) {
+            ListNode *root = new ListNode(0);
+            ListNode *cur = root;
+
+            while (s1 != nullptr && s2 != nullptr) {
+                if (s1->val < s2->val) {
+                    cur->next = s1;
+                    s1 = s1->next;
+                    cur = cur->next;
+                } else {
+                    cur->next = s2;
+                    s2 = s2->next;
+                    cur = cur->next;
+                }
+            }
+
+            while (s1 != nullptr) {
+                cur->next = s1;
+                s1 = s1->next;
+                cur = cur->next;
+            }
+
+            while (s2 != nullptr) {
+                cur->next = s2;
+                s2 = s2->next;
+                cur = cur->next;
+            }
+
+            return root->next;
+        }
         cur->next = s1;
         s1 = s1->next;
         cur = cur->next;
