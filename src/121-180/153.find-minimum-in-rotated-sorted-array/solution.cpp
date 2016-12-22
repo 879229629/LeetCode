22a89acbf088e635a4f0064a882388c31d9d160f
steven <zenggang@bangwork.com>
2016-12-22 22:49:38 +0800

    优化

--- src/121-180/153.find-minimum-in-rotated-sorted-array/solution.cpp
+++ src/121-180/153.find-minimum-in-rotated-sorted-array/solution.cpp
@@ -1,19 +1,13 @@
 #include "solution.h"
 
 int solution::findMin(std::vector<int> &nums) {
-    if (nums.empty()) return 0;
     int i = 0;
     int j = nums.size() - 1;
-    while (i <= j) {
+    while (i < j) {
+        if (nums[i] < nums[j]) return nums[i];
         int mid = i + (j - i) / 2;
-        if (mid == i) break;
-        if (nums[mid] > nums[i]) {
-            if (nums[i] > nums[j]) i = mid + 1;
-            else j = mid;
-        } else {
-            if (nums[mid] < nums[j]) j = mid;
-            else i = mid + 1;
-        }
+        if (nums[i] <= nums[mid]) i = mid + 1;
+        else j = mid;
     }
-    return std::min(nums[i], nums[j]);
+    return nums[i];
 }
