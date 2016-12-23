0267e7ecf0d9609404a183054106f816ec6ef9b0
steven <zenggang@bangwork.com>
2016-12-23 09:36:14 +0800

    优化

--- src/121-180/162.find-peak-element/solution.cpp
+++ src/121-180/162.find-peak-element/solution.cpp
@@ -1,11 +1,15 @@
 #include "solution.h"
 
 int solution::findPeakElement(std::vector<int> &nums) {
-    if (nums.empty()) return 0;
-    int pos = 0;
-    for (int i = 1; i < nums.size(); ++i) {
-        if (nums[i] >= nums[pos]) pos = i;
-        else return pos;
+    int i = 0;
+    int j = nums.size() - 1;
+    while (i < j) {
+        int mid = i + (j - i) / 2;
+        if (nums[mid] < nums[mid + 1]) {
+            i = mid + 1;
+        } else {
+            j = mid;
+        }
     }
-    return pos;
+    return i;
 }
