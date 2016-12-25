e008cc101dfb70e2f2008870a82e079a5ba2d1ee
steven <zenggang@bangwork.com>
2016-12-25 22:41:33 +0800

    优化

--- src/181-240/190.reverse-bits/solution.cpp
+++ src/181-240/190.reverse-bits/solution.cpp
@@ -2,25 +2,11 @@
 #include <iostream>
 
 uint32_t solution::reverseBits(uint32_t n) {
-    uint32_t res = n;
-
-    int i = 0;
-    int j = 31;
-    while (i < j) {
-//        std::cout << i << "," << j << "\n";
-        bool a1 = (n >> i) % 2 == 0;
-        bool a2 = (n >> j) % 2 == 0;
-        if ((a1 && !a2) || (!a1 && a2)) {
-            if (a1) {
-                res ^= 1 << j;
-                res |= 1 << i;
-            } else {
-                res |= 1 << j;
-                res ^= 1 << i;
-            }
-        }
-        ++i;
-        --j;
+    uint32_t res = 0;
+    for (int i = 0; i < 32; ++i) {
+        res <<= 1;
+        res |= n & 1;
+        n >>= 1;
     }
     return res;
 }
