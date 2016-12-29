b068a8f75e2a6586294465676aa6e867faf5905d
steven <zenggang@bangwork.com>
2016-12-29 21:40:46 +0800

    优化

--- src/181-240/217.contains-duplicate/solution.cpp
+++ src/181-240/217.contains-duplicate/solution.cpp
@@ -3,10 +3,5 @@
 #include <set>
 
 bool solution::containsDuplicate(std::vector<int> &nums) {
-    std::set<int> s;
-    for (int i = 0; i < nums.size(); ++i) {
-        if (s.find(nums[i]) != s.end()) return true;
-        s.insert(nums[i]);
-    }
-    return false;
+    return nums.size() > std::set<int>(nums.begin(), nums.end()).size();
 }
