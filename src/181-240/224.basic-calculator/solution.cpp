64d02013a299a80712673c818d84b16ee305f077
steven <zenggang@bangwork.com>
2016-12-30 19:38:16 +0800

    优化

--- src/181-240/224.basic-calculator/solution.cpp
+++ src/181-240/224.basic-calculator/solution.cpp
@@ -6,40 +6,31 @@
 // "(1+(4+5+2)-3)+(6+8)"
 
 int solution::calculate(std::string s) {
-    std::stack<std::string> t;
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] == ' ') continue;
-        if (s[i] == '(' || s[i] == '+' || s[i] == '-') {
-            t.push(std::string(1, s[i]));
-            continue;
-        }
-        std::string f;
-        if (s[i] == ')') {
-            f = t.top();
-            t.pop();
-            t.pop();
+    std::stack<int> nums, op;
+    int num = 0;
+    int res = 0;
+    int flag = 1;
+    for (char c:s) {
+        if (isdigit(c)) {
+            num = num * 10 + c - '0';
         } else {
-            int start = i;
-            while (i + 1 < s.length() && s[i + 1] >= '0' && s[i + 1] <= '9') ++i;
-            f = s.substr(start, i - start + 1);
-        }
-        //
-        if (t.empty()) {
-            t.push(f);
-        } else if (t.top() == "(") {
-            t.push(f);
-        } else {
-            std::string op;
-            op = t.top();
-            t.pop();
-            std::string second;
-            second = t.top();
-            t.pop();
-            std::string third = op == "-" ? std::to_string(std::stoi(second) - std::stoi(f)) : std::to_string(
-                    std::stoi(second) + std::stoi(f));
-            t.push(third);
+            res += flag * num;
+            num = 0;
+            if (c == '+') flag = 1;
+            if (c == '-') flag = -1;
+            if (c == '(') {
+                nums.push(res);
+                op.push(flag);
+                res = 0;
+                flag = 1;
+            }
+            if (c == ')' && op.size() > 0) {
+                res = op.top() * res + nums.top();
+                op.pop();
+                nums.pop();
+            }
         }
     }
-    std::cout << "size:" << t.size() << "][" << t.top() << "]\n";
-    return std::stoi(t.top());
+    res += flag * num;
+    return res;
 }
